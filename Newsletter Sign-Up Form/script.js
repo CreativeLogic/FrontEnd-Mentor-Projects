ff6559e67e0820fffbb7f023096e51e6e69e8095
CreativeLogic <109026578+CreativeLogic@users.noreply.github.com>
2024-06-12 06:45:27 -0400

    Add files via upload

---
+++ Newsletter Sign-Up Form/script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const form = document.querySelector('#form');
+const signUpSection = document.querySelector('.sign-up');
+const picture = document.querySelector('picture');
+const confirmationSection = document.querySelector('.confirmation-section');
+const span = confirmationSection.querySelector('span');
+const dismissButton = confirmationSection.querySelector('button');
+const emailInput = document.querySelector('#email');
+
+const submitEmail = e => {
+  e.preventDefault();
+  const emailValue = new FormData(e.target).get('email');
+
+  if (emailValue == '' || !validateEmail(emailValue)) {
+    form.classList.add('error');
+  } else {
+    form.classList.remove('error');
+    console.log('email valid');
+    signUpSection.style.display = 'none';
+    confirmationSection.style.display = 'flex';
+    picture.style.display = 'none';
+    span.textContent = emailValue;
+    confirmationSection.removeAttribute('hidden');
+    confirmationSection.setAttribute('aria-hidden', 'false');
+  }
+};
+
+const validateEmail = email => {
+  const emailRegex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return emailRegex.test(email);
+};
+
+const checkEmailInput = e => {
+  const input = e.target;
+  if (validateEmail(input.value)) {
+    input.style.outline = '2px solid #4BB543';
+    input.style.border = 'none';
+    form.classList.remove('error');
+  } else {
+    input.style.outline = '';
+  }
+};
+
+const returnToSignUp = () => {
+  signUpSection.style.display = 'flex';
+  picture.style.display = 'block';
+  confirmationSection.style.display = 'none';
+  form.reset();
+  confirmationSection.setAttribute('hidden', 'true');
+  emailInput.style.outline = 'var(--input-border)';
+  confirmationSection.setAttribute('aria-hidden', 'true');
+};
+
+form.addEventListener('submit', submitEmail);
+dismissButton.addEventListener('click', returnToSignUp);
+emailInput.addEventListener('input', checkEmailInput);
