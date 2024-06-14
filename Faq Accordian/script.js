836760a86b6a24052d6165ae50d930b7dbb498bc
CreativeLogic <109026578+CreativeLogic@users.noreply.github.com>
2024-06-14 14:28:44 -0400

    Update script.js

--- Faq Accordian/script.js
+++ Faq Accordian/script.js
@@ -2,10 +2,9 @@ const summaries = document.querySelectorAll('summary');
 
 const toggleIcon = e => {
   const summary = e.currentTarget;
-  const details = summary.closest('details');
   const icon = summary.querySelector('img');
 
-  if (details.open) {
+  if (icon.src.includes('plus')) {
     icon.src = './assets/images/icon-minus.svg';
   } else {
     icon.src = './assets/images/icon-plus.svg';
